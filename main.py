88ea9135ba92ccf3d36ff1edaa78a6df097dab6a
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-09-27 15:26:54 -0400

    add members

--- main.py
+++ main.py
@@ -19,6 +19,21 @@ service = build('sheets', 'v4', http=credentials.authorize(Http()))
 wb = gc.open_by_key(secrets.google_sheet_id)
 wks = wb.worksheet('Report')
 
+# Table to organize trello card members by id
+members = {}
+
+def get_trello_member(member_id):
+  try:
+    member_name = members[member_id]
+  except KeyError:
+    member_json =  requests.request('GET', trello.url_members + member_id + trello.tokens).json()
+    member_name = member_json['fullName']
+    members[member_id] = member_name
+  return {
+    'id': member_id,
+    'name': member_name,
+  }
+
 def main_script():
   # Duplicate the Report worksheet
   today = dt.datetime.today().strftime('%m-%d-%y')
@@ -46,13 +61,19 @@ def main_script():
       for c in list_json:
         x += 1
         print(str(x) + ' - ' + c['name'])
+        card_members = []
+        if len(c['idMembers']) > 0:
+          for id in c['idMembers']:
+            card_member = get_trello_member(id)
+            card_members.append(card_member['name'])
         new_row = [
           b['name'],
           l['name'],
           c['name'],
           c['shortUrl'],
           str(dt.datetime.fromtimestamp(int(c['id'][0:8],16))),
-          str(dt.datetime.strptime(c['dateLastActivity'], '%Y-%m-%dT%H:%M:%S.%fZ'))
+          str(dt.datetime.strptime(c['dateLastActivity'], '%Y-%m-%dT%H:%M:%S.%fZ')),
+          ', '.join(card_members)
         ]
 
         # Append card / ticket to the table
